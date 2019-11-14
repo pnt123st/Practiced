bda87ae3ae2db3727f049c06c3fa76ca03d6b333
pnt123st <imannanr@gmail.com>
2019-11-14 01:58:18 -0500

    Modified Code

--- W3SchoolTableData/src/main/java/table/page/TablePage.java
+++ W3SchoolTableData/src/main/java/table/page/TablePage.java
@@ -7,20 +7,26 @@ import org.openqa.selenium.support.How;
 public class TablePage  {
 
     //Retrieve entire table
-    /*@FindBy(how = How.CSS,using = ".w3-responsive")
-    public WebElement tableElement;*/
+    @FindBy(how = How.CSS,using = ".w3-responsive")
+    public WebElement tableElement1;
 
     //Reading 2nd row
-    /*@FindBy(how = How.CSS,using = ".w3-responsive tr:nth-child(3)")
-    public WebElement tableElement;*/
+    @FindBy(how = How.CSS,using = ".w3-responsive tr:nth-child(3)")
+    public WebElement tableElement2;
 
     @FindBy(how = How.CSS,using = ".w3-responsive tr:nth-child(3) td:nth-child(7)")
-    public WebElement tableElement;
+    public WebElement tableElement3;
 
-    public void getTableData(){
-        String tableData = tableElement.getText();
-        System.out.println(tableData);
+    public void getTableData1() {
+        String tableData1 = tableElement1.getText();
+        System.out.println(tableData1);
     }
-
-
+    public void getTableData2() {
+        String tableData2 = tableElement2.getText();
+        System.out.println(tableData2);
+    }
+    public void getTableData3(){
+        String tableData3 = tableElement3.getText();
+        System.out.println(tableData3);
+    }
 }
--- W3SchoolTableData/src/test/java/table/data/TestTableData.java
+++ W3SchoolTableData/src/test/java/table/data/TestTableData.java
@@ -10,6 +10,8 @@ public class TestTableData extends CommonAPI {
     @Test
     public void scrapeTable(){
         TablePage tablePage = PageFactory.initElements(driver,TablePage.class);
-        tablePage.getTableData();
+        tablePage.getTableData1();
+        tablePage.getTableData2();
+        tablePage.getTableData3();
     }
 }
