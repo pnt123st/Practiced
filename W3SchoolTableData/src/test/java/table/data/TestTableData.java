b8d36d9689d0e1d5a22c788ff85263dcdcde0655
pnt123st <imannanr@gmail.com>
2019-11-14 02:43:30 -0500

    Removed tablePage.getTableData

--- W3SchoolTableData/src/test/java/table/data/TestTableData.java
+++ W3SchoolTableData/src/test/java/table/data/TestTableData.java
@@ -12,6 +12,6 @@ public class TestTableData extends CommonAPI {
         TablePage tablePage = PageFactory.initElements(driver,TablePage.class);
         tablePage.getTableData1();
         tablePage.getTableData2();
-        tablePage.getTableData3();
+       
     }
 }
